96dcb969d85b92dc3ce2fa4fa4737accec311412
networkdavit <lonecreation1612@gmail.com>
2023-03-11 01:53:51 +0400

    Initial Commit

---
+++ CatsAPP/CatsAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsAPPApp.swift
+//  CatsAPP
+//
+//  Created by Davit Hayrapetyan on 11.03.23.
+//
+
+import SwiftUI
+
+@main
+struct CatsAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
